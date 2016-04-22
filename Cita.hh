232caa2863f976a69262ee493b1543fc63b87092
rabbitwhile <vallsarnau@gmail.com>
2016-04-22 17:33:24 +0200

    He pujat el fitxer Cita.hh

---
+++ Cita.hh
@@ -0,0 +1,41 @@
+/** @file Cita.hh
+	@brief Classe Cita
+*/
+
+#ifndef CITA_HH
+#define CITA_HH
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Cita{
+
+	private:
+
+	public:
+		/* Constructores */
+
+		/** @brief Creadora per defecte.
+			\pre cert
+			\post El resultat es una nova cita
+		*/ 
+		Cita();
+	
+		/* Destructora */
+		/** @brief Destructora per defecte.
+			\pre Existeixi un objecte text
+			\post Destrueix l'objecte tipus cita
+		*/
+		~Cita();
+	
+		/*Consultores*/
+		
+		/* Modificadores*/
+	
+		
+
+};
+
+#endif

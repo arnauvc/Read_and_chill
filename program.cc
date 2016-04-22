ee27bf775aa2bfaebdfe9add4c3d83111eeb6765
rabbitwhile <vallsarnau19@gmail.com>
2016-04-22 19:57:08 +0200

    program i llegeixme

--- program.cc
+++ program.cc
@@ -1,3 +1,10 @@
+/**
+ * @mainpage Pràctica PRO2 - Gestor de textos i cites
+ 
+/** @file program.cc
+    @brief Programa principal de <em>Pràctica PRO2 - Gestor de textos i cites</em>.
+*/
+
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -5,8 +12,17 @@
 
 using namespace std;
 
+/** @brief Programa principal.
+*/
+
 int main(){
 	Biblioteca biblio;
+	string opcio;
+	
+	getline(cin,linia);
+    iss.str(linia);
+    iss.seekg(0);
+    iss >> op;
 
 }
 

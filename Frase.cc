6d3bc29d6ff0d472838ec4d9ccd06a1dc7d56de7
omaralshaeb <omar.alshaeb@gmail.com>
2016-05-09 21:36:25 +0200

    falta acabarla

--- Frase.cc
+++ Frase.cc
@@ -1,15 +1,33 @@
 #include "Frase.hh"
 
-Frase::Frase(){
-    
+/* Constants */
+
+const char *ER1 = "error";
+#include <exception>
+
+Frase::Frase() {
+	num_paraules = 0;
 }
 
-Frase::~Frase(){
-    
+Frase::~Frase() {}
+
+void Frase::llegir_frase() {
+    string s;
+    while (cin >> s and s != ".") { //falta mirar si nomes es acabat en punt o amb mes signes de puntuacio
+    	frase_in.push_back(s);
+    	++num_paraules;
+    }
 }
 
-
-
-void Frase::llegir_frase(){
-    
+void Frase::escriu_frase() {
+	int x = frase_in.size();
+	bool primer = true;
+	for (int i = 0; i < x; ++i) {
+		if (primer) {
+			cout << frase_in[i];
+			primer = false;
+		}
+		else cout << " " << frase_in[i];
+	}
+	cout << endl;
 }

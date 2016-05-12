d6c9a2da9a971599685305918e028bd17e05896c
omaralshaeb <omar.alshaeb@gmail.com>
2016-05-12 12:04:15 +0200

    Update Text.cc

--- Text.cc
+++ Text.cc
@@ -68,10 +68,25 @@ void const Text::paraules_frase(string s1){
     }
 }
 
+bool const Text::buscar_paraules(string s) {
+    int x = autor.size();
+    int y = titol.size();
+    int z = s.size();
+    for (int i = 0; i < x; ++i) {
+        //autor
+    }
+    for (int i = 0; i < y; ++i) {
+        //titol
+    }
+    for (int i = 0; i < x; ++i) {
+        //contingut
+    }
+    return true;
+}
+
 void Text::substitueix_paraules(string s1, string s2){
 	cout << "0" << endl;
     for(map<int,Frase>::const_iterator i = contingut.begin(); i != contingut.end(); ++i){
-		cout << "1" << endl;
         Frase f = i->second;
 		int num = i->first;
         f.canvi_paraules(s1,s2);
@@ -82,16 +97,21 @@ void Text::substitueix_paraules(string s1, string s2){
 }
 
 void const Text::expressio_frases(string s1){//portara feina
-	
+    int j;
+    for(map<int,Frase>::const_iterator i = contingut.begin(); i != contingut.end(); ++i) {
+        Frase f = i->second;
+        j = 0;
+        if (compleix_expressio(s1, j)) {
+            f.escriu_frase();
+        }
+    }
 }
 
-
-
-void const Text::taula_frequencies(){
+/*void const Text::taula_frequencies(){
     for(int i = 0; i < taulafreq.size(); ++i){
         cout << taulafreq[i].second << " " << taulafreq[i].first <<endl;
     }
-}
+}*/
 
 void Text::llegir_text(){ // falta definir l'acabament de la lectura, La lectora de Frase podria retornar un 0, o 1 segons si detecta *** o no.
     
@@ -116,3 +136,37 @@ void Text::llegir_text(){ // falta definir l'acabament de la lectura, La lectora
     
     //sort(taulafreq.begin(), taulafreq.end(), sort_comp);
 }
+//....................................................................................................................
+// Retorna true si el paràmetre implicit compleix l'expressio exp, fals altrament
+// "exp" es l'expressió, "i" és la posició de l'element actual (comencem a 0, primer element de exp)
+/*bool Tasca::compleix_expressio(const string &exp, int &i) {
+    if (exp[i]!='(') {  //Si l'element no es un parentesi, l'analitzem
+        string e;   //Si es una etiqueta, es guardara a "e"
+        while (exp[i]!='|' and exp[i]!='&' and exp[i]!=')') { //Si es una etiqueta (no es '&' o ',' o ')') la guardem 
+                                      // a "e" i avancem posicions (i) fins trobar un caracter 
+                                      // que no pertanyi a l'etiqueta ('.' o ',' o ')') 
+            e.push_back(exp[i]);    // fem push_back per afegir la nova lletra al final del string "e"
+            ++i;            //avançem posicio
+        }
+        return (conte_etiqueta_simple(e)); //Utilitzem la funcio per saber si la tasca conte l'etiqueta "e", 
+                           // retorna fals si no.
+    } else { // Si es un parentesi '(', es el principi d'una expressio booleana
+        ++i;  //Avancem una posicio, podrem trobar un altre parentesi (una altre expressio) o una etiqueta
+        bool r1 = compleix_expressio(exp,i);  //retorna cert si es una etiqueta i aquesta pertany a la tasca,
+                              // fals altrament. Si no es una etiqueta, retorna cert si la següent 
+                              // expressió trobada es certa.
+        char c = exp[i];    // c=element actual, element que va despres de l'etiqueta trobada a la
+                    // linia anterior. Nomes pot ser "&" o ",".
+        ++i;            // Avancem una posicio, hem de trobar nova etiqueta o un parentesi (nova expressio)
+        bool r2 = compleix_expressio(exp,i); //retorna cert si es una etiqueta i aquesta pertany a la tasca, 
+                             // fals altrament. Si no es una etiqueta retorna cert si la següent
+                             // expressió trobada es certa.
+        ++i;            //Avancem una posicio, per que "i" retorni el valor adequat (sempre 
+                    // una posicio despres de l'element analitzat en aquesta iteracio).
+        if (c=='&') {       // Haviem guardat "&" o "," a "c". 
+            return (r1 and r2); //Si es "&" es un and, retorna true si es compleix
+        } else {
+            return (r1 or r2);  //Si es "|" es un or, retorna true si es compleix
+        }
+    }
+}*/

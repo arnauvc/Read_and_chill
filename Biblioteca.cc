671896933af621b45ff975df505cfd41bed6be38
omaralshaeb <omar.alshaeb@gmail.com>
2016-05-12 16:13:56 +0200

    Update Biblioteca.cc

--- Biblioteca.cc
+++ Biblioteca.cc
@@ -1,6 +1,7 @@
 #include "Biblioteca.hh"
 #include "Text.hh"
 #include "Frase.hh"
+#include <algorithm>
 
 Biblioteca::Biblioteca(){
     triat = false;
@@ -11,20 +12,18 @@ Biblioteca::~Biblioteca(){
 }
 
 Text const Biblioteca::triar_text(string s, bool &b){
-	
 	for (map<string,Text>::const_iterator i = conjunt_textos.begin(); i != conjunt_textos.end(); ++i) {
 		Text t = i->second;
-	     t.paraules_frase(s);
+		if (t.buscar_paraules(s)) {
 			ttriat = i->second;
 			triat = true;
 			b = true;
 			return ttriat;
-		
+		}
 	}
 	if (not triat) cout << "error" << endl;
 	b = false;
 	return ttriat;
-	
 }
 
 bool const Biblioteca::consultar_triat(){
@@ -32,15 +31,43 @@ bool const Biblioteca::consultar_triat(){
 }
 
 void const Biblioteca::tots_textos(){
-    
+	Text t;
+	string s;
+    for (map<string,Text>::const_iterator i = conjunt_textos.begin(); i != conjunt_textos.end(); ++i) {
+    	t = i->second;
+    	s = i->first;
+    	cout << s << " " << t.titol_text() << endl;
+    }
+    cout << "11111111111111111111111111" << endl;
 }
 
-void const Biblioteca::textos_autor(){
-    
+void const Biblioteca::textos_autor(string s){
+	vector<string> titols_autor;
+	Text t;
+    for (map<string,Text>::const_iterator i = conjunt_textos.begin(); i != conjunt_textos.end(); ++i) {
+    	if (i->first == s) {
+    		t = i->second;
+    		titols_autor.push_back(t.titol_text());
+    	}
+    }
+    sort(titols_autor.begin(), titols_autor.end(), comp_titols);
+    for (int i = 0; i < titols_autor.size(); ++i) {
+    	cout << titols_autor[i] << endl;
+    }
+    cout << "22222222222222222222222" << endl;
 }
 
 void const Biblioteca::tots_autors(){
 	//RAPID!
+	string s;
+	infoautor k;
+	for (map<string, infoautor>::const_iterator i = conjunt_autors.begin(); i != conjunt_autors.end(); ++i) {
+		k = i->second;
+		s = i->first;
+		cout << s << " " << k.ntextos << " ";
+		cout << k.nfrases << " " << k.nparaules << endl;
+    }
+    cout << "3333333333333333333333" << endl;
 }
 
 void Biblioteca::afegir_text(){
@@ -51,17 +78,41 @@ void Biblioteca::afegir_text(){
     //hem de buscar que no hi hagi cap autor amb el mateix titol que el nou text afegit sino error 
 }
 
-void Biblioteca::afegir_cita(int x, int y){
-	/*
-	char first, last;
-	first = cin.get();     // get one character
-	cin.ignore(256,' ');   // ignore until space
-	last = cin.get();      // get one character
-	*/
-}
-
 void Biblioteca::eliminar_text(){
     //map<string, Text>::const_iterator i = find(ttriat.autor_text());
     conjunt_textos.erase(ttriat.autor_text());
     triat = false;
 }
+
+void Biblioteca::afegir_cita(int x, int y){
+	
+	/*char first, last;
+	first = cin.get();     // get one character
+	cin.ignore(256,' ');   // ignore until space
+	last = cin.get();*/      // get one character
+	
+}
+
+/*void const Biblioteca::cites_autor(string autor) {
+	for (map<string,Text>::const_iterator i = conjunt_cites.begin(); i != conjunt_cites.end(); ++i) {
+		
+    }
+}
+
+void const Biblioteca::info_cita(string referencia) {
+
+}
+
+void const Biblioteca::totes_cites() {
+	for (map<string,Text>::const_iterator i = conjunt_cites.begin(); i != conjunt_cites.end(); ++i) {
+
+    }
+}
+
+void Biblioteca::eliminar_cita(string referencia) {
+	//map<string, Text>::const_iterator i = find(ttriat.autor_text());
+    conjunt_cites.erase(ttriat.autor_text());
+}*/
+
+
+		
